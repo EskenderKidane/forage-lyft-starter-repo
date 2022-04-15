7380d8fae44d1c6c40b2948ea06fff02ec989464
Eskender <eskender.araia@hotmail.com>
2022-04-15 19:47:31 +0300

    Refactor

---
+++ Battery/battery.py
@@ -0,0 +1,22 @@
+from abc import ABC
+
+from car import Car
+
+
+class SpindlerBattery(Car, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def battery_should_be_serviced(self):
+        return self.current_date - self.last_service_date > 2
+
+class NubbinBattery(Car, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def battery_should_be_serviced(self):
+        return self.current_date - self.last_service_date > 4
---
+++ carFactory/carFactory.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+from engine.engine import CapuletEngine, WilloughbyEngine, SternmanEngine
+
+
+class Calliope(CapuletEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class Glissade(WilloughbyEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class Palindrome(SternmanEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class Rorschach(WilloughbyEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
+class Thovex(CapuletEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
