d90169e270408283c2d9688a67b265cdf01ac636
Eskender <eskender.araia@hotmail.com>
2022-04-23 00:03:14 +0300

    Refactor 2.0 and add new unit tests

--- Battery/battery.py
+++ Battery/battery.py
@@ -1,22 +1,21 @@
 from abc import ABC
 
-from car import Car
+class Battery(ABC):
+    def needs_service(self):
+        pass
 
-
-class SpindlerBattery(Car, ABC):
+class SpindlerBattery(Battery):
     def __init__(self, last_service_date, current_date):
-        super().__init__(last_service_date)
         self.current_date = current_date
         self.last_service_date = last_service_date
 
-    def battery_should_be_serviced(self):
-        return self.current_date - self.last_service_date > 2
+    def needs_service(self):
+        return self.current_date - self.last_service_date >= 2
 
-class NubbinBattery(Car, ABC):
+class NubbinBattery(Battery):
     def __init__(self, last_service_date, current_date):
-        super().__init__(last_service_date)
         self.current_date = current_date
         self.last_service_date = last_service_date
 
-    def battery_should_be_serviced(self):
-        return self.current_date - self.last_service_date > 4
+    def needs_service(self):
+        return self.current_date - self.last_service_date >= 4
